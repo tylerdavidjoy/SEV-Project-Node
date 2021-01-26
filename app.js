f658ebf137cf34b15ce5e6106b431c95ce09f121
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2021-01-25 21:52:10 -0600

    Set up basic node app

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
+/* var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users'); */
+
+var express = require('express');
+const bodyParser = require("body-parser");
+const mysql = require('mysql');
 
 var app = express();
+const PORT = 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to The Church Management System." });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// set port, listen for requests
+app.listen(PORT, () => {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(process.env.NODE_ENV == "development") //Reminder during development, does not run deployed
+    console.log("Make sure Apache and MySQL are running via XAMPP");
+
+  console.log("Server is running on port " + PORT + ".");
 });
-
-module.exports = app;
