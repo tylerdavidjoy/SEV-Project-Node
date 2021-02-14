79a97ff654445b4ebc44aad71fc87ac40e8ee464
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2021-02-14 15:29:20 -0600

    Added phone_number and person_number API's.

---
+++ models/person_number.model.js
@@ -0,0 +1,75 @@
+const sql = require("./db.js");
+
+// Constructor
+const Person_Number = function(person_number) {
+    this.person_ID = person_number.person_ID;
+    this.number_ID = person_number.number_ID;
+}
+
+Person_Number.create = (person_number, result) => {
+    sql.query(`INSERT INTO person_number VALUES (${person_number.person_ID}, ${person_number.number_ID} )`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        } else {
+            result(null, person_number);
+        }
+    })
+}
+
+Person_Number.findAll = result => {
+    sql.query("SELECT * FROM person_number", (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+        console.log("person_number: ", res);
+        result(null, res);
+    })
+}
+
+//Find by Person_ID
+Person_Number.findById = (id, result) => {
+    sql.query(`SELECT * FROM person_number WHERE person_number.person_ID = "${id}"`, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      if (res.length) {
+        console.log("found person_numbers: ", res[0]);
+        result(null, res[0]);
+        return;
+      }
+  
+      // not found person_number with the id
+      result({ kind: "not_found" }, null);
+    })
+}
+
+Person_Number.remove = (id, result) => {
+    sql.query(`DELETE FROM person_number WHERE number_ID = "${id}"`, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      if (res.affectedRows == 0) {
+        // not found person_number with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      
+      else {
+        console.log("deleted person_number(s) with number_ID: ", id);
+        result(null, res);
+      }
+
+    });
+  };
+
+module.exports = Person_Number;
--- routes/church.routes.js
+++ routes/church.routes.js
@@ -1,6 +1,8 @@
 module.exports = app => {
     const valid_value = require("../controllers/valid_value.controller.js");
     const congregation = require("../controllers/congregation.controller.js");
+    const phone_number = require("../controllers/phone_number.controller.js");
+    const person_number = require("../controllers/person_number.controller.js");
 
     // ----------------------------------
     // valid_value Table API
@@ -34,4 +36,38 @@ module.exports = app => {
 
     // Delete a congregation
     app.delete("/congregation", congregation.delete);
+
+    
+    // ----------------------------------
+    // phone_number Table API
+    // ----------------------------------
+
+    //Create a new phone_number
+    app.post("/phone_number", phone_number.create);
+
+    // Find phone_number(s) by a parameter
+    app.get("/phone_number", phone_number.find);
+
+    // Update a phone_number
+    app.put("/phone_number", phone_number.update);
+
+    // Delete a phone_number
+    app.delete("/phone_number", phone_number.delete);
+
+    
+    // ----------------------------------
+    // person_number Table API
+    // ----------------------------------
+
+    //Create a new person_number
+    app.post("/person_number", person_number.create);
+
+    // Find person_number(s) by a parameter
+    app.get("/person_number", person_number.find);
+
+    // Update a person_number
+    app.put("/person_number", person_number.update);
+
+    // Delete a person_number
+    app.delete("/person_number", person_number.delete);
 };
