afa85f1a6cdf11cc1e9f5cc4807292737a0437f9
Tyler Joy <35614944+tylerdavidjoy@users.noreply.github.com>
2021-02-17 11:55:41 -0600

    Add person.sql

---
+++ sql_files/person.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `person` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `congregation_ID` int(11) NOT NULL,
+  `f_name` varchar(45) NOT NULL,
+  `l_name` varchar(45) NOT NULL,
+  `occupation` varchar(255) DEFAULT NULL,
+  `employer` varchar(255) DEFAULT NULL,
+  `family_ID` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
