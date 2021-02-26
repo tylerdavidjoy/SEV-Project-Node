dfd1a504f33e704a19c4328863e19f55bc4f14f9
michaelspringer250 <mspringer251@gmail.com>
2021-02-26 12:09:42 -0600

    address and phone_number delete when unreferenced

--- models/address.model.js
+++ models/address.model.js
@@ -1,15 +1,14 @@
 const sql = require("./db.js");
 
 // Constructor
-const Address = function(address) {
+const Address = function (address) {
     this.ID = address.ID;
     this.address = address.address;
     this.type = address.type;
 }
 
 Address.create = (address, person_ID, result) => {
-    let addressPromise = new Promise(function(addressResolve, addressReject)
-    {
+    let addressPromise = new Promise(function (addressResolve, addressReject) {
         sql.query(`INSERT INTO church.address SET address = "${address.address}", type = "${address.type}"`, (err, res) => {
             if (err) {
                 console.log("error: ", err);
@@ -22,11 +21,10 @@ Address.create = (address, person_ID, result) => {
 
     })
     addressPromise.then(
-        function(response) {
-            if(person_ID != null)
-            {
+        function (response) {
+            if (person_ID != null) {
                 sql.query(`INSERT INTO church.person_address SET person_ID = "${person_ID}", address_ID = "${response}"`, (err, res) => {
-                    if(err) {
+                    if (err) {
                         console.log("error: ", err);
                         result(err, null);
                     } else {
@@ -38,7 +36,7 @@ Address.create = (address, person_ID, result) => {
                 result(null, address);
             }
         },
-        function(error) {
+        function (error) {
             result(error, null);
         }
     )
@@ -47,9 +45,9 @@ Address.create = (address, person_ID, result) => {
 Address.findAll = result => {
     sql.query("SELECT * FROM address", (err, res) => {
         if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
+            console.log("error: ", err);
+            result(err, null);
+            return;
         }
         console.log("addresses: ", res);
         result(null, res);
@@ -58,37 +56,37 @@ Address.findAll = result => {
 
 Address.findById = (id, result) => {
     sql.query(`SELECT * FROM address WHERE address.ID = "${id}"`, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-  
-      if (res.length) {
-        console.log("found address: ", res[0]);
-        result(null, res[0]);
-        return;
-      }
-  
-      // not found address with the id
-      result({ kind: "not_found" }, null);
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found address: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        // not found address with the id
+        result({ kind: "not_found" }, null);
     })
 }
 
 Address.findByPersonID = (person_ID, result) => {
     sql.query(`SELECT * FROM address WHERE address.ID IN (SELECT address_ID FROM person_address WHERE person_address.person_ID = "${person_ID}")`, (err, res) => {
         if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
+            console.log("error: ", err);
+            result(err, null);
+            return;
         }
-    
+
         if (res.length) {
-          console.log("found addresses: ", res);
-          result(null, res);
-          return;
+            console.log("found addresses: ", res);
+            result(null, res);
+            return;
         }
-    
+
         // not found address with value_group
         result({ kind: "not_found" }, null);
     })
@@ -96,40 +94,39 @@ Address.findByPersonID = (person_ID, result) => {
 
 
 Address.updateById = (id, address, result) => {
-    sql.query(`UPDATE address SET address = "${address.address}", type = "${address.type}" WHERE ID = "${id}"`,(err, res) => {
+    sql.query(`UPDATE address SET address = "${address.address}", type = "${address.type}" WHERE ID = "${id}"`, (err, res) => {
         if (err) {
             console.log("error: ", err);
             result(err, null);
             return;
         }
-  
+
         if (res.affectedRows == 0) {
             // not found address with the id
             result({ kind: "not_found" }, null);
             return;
         }
         result(null, res);
-        }
+    }
     );
 }
 
 Address.remove = (id, result) => {
     sql.query(`DELETE FROM address WHERE ID = "${id}"`, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
-      if (res.affectedRows == 0) {
-        // not found address with the id
-        result({ kind: "not_found" }, null);
-        return;
-      }
-  
-      console.log("deleted address with id: ", id);
-      result(null, res);
-    });
-  };
+        if (err) {
+            result(null, err);
+            return;
+        } else if (res.affectedRows == 0) {
+            // not found address with the id
+            result({ kind: "not_found" }, null);
+            return;
+        } else {
+            console.log("deleted address with id: ", id);
+            result(null, res);
+        }
+    })
+};
+
+
 
 module.exports = Address;
--- models/person.model.js
+++ models/person.model.js
@@ -1,98 +1,138 @@
 const sql = require("./db.js");
 
 // Constructor
-const Person = function(person) {
-    this.congregation_ID = person.congregation_ID;
-    this.f_name = person.f_name;
-    this.l_name = person.l_name;
-    this.occupation = person.occupation;
-    this.employer = person.employer;
-    this.family_ID = person.family_ID;
-    this.email = person.email;
+const Person = function (person) {
+  this.congregation_ID = person.congregation_ID;
+  this.f_name = person.f_name;
+  this.l_name = person.l_name;
+  this.occupation = person.occupation;
+  this.employer = person.employer;
+  this.family_ID = person.family_ID;
+  this.email = person.email;
 }
 
 Person.create = (person, result) => {
-    sql.query(`INSERT INTO person VALUES ("", ${person.congregation_ID}, "${person.f_name}", "${person.l_name}", "${person.occupation}", "${person.employer}", ${person.family_ID}, "${person.email}")`, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(err, null);
-            return;
-        } else {
-            result(null, person);
-        }
-    })
+  sql.query(`INSERT INTO person VALUES ("", ${person.congregation_ID}, "${person.f_name}", "${person.l_name}", "${person.occupation}", 
+  "${person.employer}", ${person.family_ID}, "${person.email}")`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    } else {
+      result(null, person);
+    }
+  })
 }
 
 Person.findAll = result => {
-    sql.query("SELECT * FROM person", (err, res) => {
-        if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
-        }
-        console.log("People: ", res);
-        result(null, res);
-    })
+  sql.query("SELECT * FROM person", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    console.log("People: ", res);
+    result(null, res);
+  })
 }
 
 //Find by ID
 Person.findById = (id, result) => {
-    sql.query(`SELECT * FROM person WHERE person.ID = "${id}"`, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-  
-      if (res.length) {
-        console.log("found person: ", res);
-        result(null, res);
-        return;
-      }
-  
-      // not found person with the id
-      result({ kind: "not_found" }, null);
-    })
+  sql.query(`SELECT * FROM person WHERE person.ID = "${id}"`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found person: ", res);
+      result(null, res);
+      return;
+    }
+
+    // not found person with the id
+    result({ kind: "not_found" }, null);
+  })
 }
 
 Person.updateById = (id, person, result) => {
-  sql.query(`UPDATE person SET congregation_ID = ${person.congregation_ID}, f_name = "${person.f_name}", l_name = "${person.l_name}", occupation = "${person.occupation}", employer = "${person.employer}", family_ID = ${person.family_ID}, email = "${person.email}" WHERE ID = "${id}"`,(err, res) => {
-      if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
-      }
+  sql.query(`UPDATE person SET congregation_ID = ${person.congregation_ID}, f_name = "${person.f_name}", l_name = "${person.l_name}", 
+  occupation = "${person.occupation}", employer = "${person.employer}", family_ID = ${person.family_ID}, email = "${person.email}" 
+  WHERE ID = "${id}"`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
 
-      if (res.affectedRows == 0) {
-          // not found person with the id
-          result({ kind: "not_found" }, null);
-          return;
-      }
-      result(null, res);
-      }
+    if (res.affectedRows == 0) {
+      // not found person with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+    result(null, res);
+  }
   );
 }
 
 Person.remove = (id, result) => {
+  let personPromise = new Promise(function (personResolve, personReject) {
     sql.query(`DELETE FROM person WHERE ID = "${id}"`, (err, res) => {
       if (err) {
         console.log("error: ", err);
         result(null, err);
-        return;
+        personReject(err);
       }
-  
+
       if (res.affectedRows == 0) {
         // not found person with the id
         result({ kind: "not_found" }, null);
-        return;
+        personReject({ kind: "not_found" }, null);
       }
 
       else {
         console.log("deleted person(s) with number_ID: ", id);
         result(null, res);
+        personResolve();
       }
 
-    });
-  };
+    })
+  })
+  personPromise.then(
+    function () {
+      garbageCollection();
+    },
+    function (error) {
+      console.log("error: ", error);
+    }
+  )
+}
+
+function garbageCollection() {
+  sql.query(`DELETE FROM address WHERE NOT EXISTS (SELECT * from person_address WHERE person_address.address_ID = address.ID)
+    AND NOT EXISTS (SELECT * FROM family WHERE family.address_ID = address.ID)`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      console.log("No deleted address rows");
+      return;
+    }
+  })
+  sql.query(`DELETE FROM phone_number WHERE NOT EXISTS (SELECT * from person_number WHERE person_number.number_ID = phone_number.ID)`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      console.log("No deleted phone_number rows");
+      return;
+    }
+  })
+}
 
 module.exports = Person;
