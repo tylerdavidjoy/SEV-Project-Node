6f8a973f21280b8b2b9f35ebed4786736aa33a95
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2021-03-23 14:29:58 -0500

    Added report generation api call for life_event

--- models/life_event.model.js
+++ models/life_event.model.js
@@ -109,6 +109,25 @@ Life_Event.findbydateType = (type,start,end, result) => {
   })
 }
 
+Life_Event.generateReport = (type,start,end, result) => {
+  sql.query(`SELECT person.f_name, person.l_name, life_event.description, life_event.date FROM (person JOIN life_event) WHERE life_event.type = "${type}" AND DAYOFYEAR(life_event.date) BETWEEN DAYOFYEAR("${start}") AND DAYOFYEAR("${end}")`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found life_event(s): ", res);
+      result(null, res);
+      return;
+    }
+
+    // not found life_event with the id
+    result({ kind: "not_found" }, null);
+  })
+}
+
 Life_Event.findbydate = (start,end, result) => {
   start = String(start);
   end = String(end);
